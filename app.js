740727d870b1c29c5b17b67bea13b03e8942ba8c
munna57 <75237010+munna57@users.noreply.github.com>
2022-07-11 15:28:12 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Sucess..!')
-  res.json({ message: 'Hello, From NEPAL' })
+  res.json({ message: 'Hello, From INDIA' })
 });
 
 
