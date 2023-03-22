a0700ff3df77c43097a777b5ae8fd5ec57cfefcf
munna57 <75237010+munna57@users.noreply.github.com>
2023-03-22 16:38:21 +1100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Sucess..!')
-  res.json({ message: 'Hello, From INDIA' })
+  res.json({ message: 'Hello, Azure ver 3' })
 });
 
 
